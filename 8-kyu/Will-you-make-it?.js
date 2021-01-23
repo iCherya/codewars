05f2aac8769e7204b3b2037889c43b885d7eb6dc
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-23 14:02:59 +0200

    8 kyu - Will you make it?

---
+++ 8-kyu/Will-you-make-it?.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Will you make it?
+ * https://www.codewars.com/kata/5861d28f124b35723e00005e
+ */
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuelLeft;
