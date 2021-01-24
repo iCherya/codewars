27770ff8c0ec624665884d83bc9919e53991d850
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-24 09:43:54 +0200

    8 kyu - All Star Code Challenge #18

---
+++ 8-kyu/All-Star-Code-Challenge-#18.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - All Star Code Challenge #18
+ * https://www.codewars.com/kata/5865918c6b569962950002a1
+ */
+const strCount = (str, letter) => str.split('').filter((el) => el === letter).length;
