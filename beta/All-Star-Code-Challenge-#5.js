66a5c120392b3ed7da644562022a8b820a16f70c
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-25 12:17:51 +0200

    Beta - All Star Code Challenge #5

---
+++ beta/All-Star-Code-Challenge-#5.js
@@ -0,0 +1,5 @@
+/**
+ * Beta - All Star Code Challenge #5
+ * https://www.codewars.com/kata/5864152183f7e6e834000160
+ */
+const randomMovies = (movies) => movies[Math.floor(movies.length * Math.random())];
