cbd203bd19018af401d9cb2fddc0d11b96a2825f
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-25 14:14:05 +0200

    7 kyu - Vowel Count fix

--- 7-kyu/Vowel-Count.js
+++ 7-kyu/Vowel-Count.js
@@ -2,9 +2,4 @@
  * 7 kyu - Vowel Count
  * https://www.codewars.com/kata/54ff3102c1bad923760001f3
  */
-const getCount = (str) => {
-    const vowels = /[aeiou]/;
-
-    return str.split(vowels).length - 1;
-};
-console.log(getCount('abracadabra'));
+const getCount = (str) => str.split(/[aeiou]/).length - 1;
