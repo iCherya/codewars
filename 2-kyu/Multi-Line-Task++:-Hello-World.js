8348ded3001d28e1b3cdc47b9747abd7fc8d8749
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-26 07:27:40 +0200

    2 kyu - Multi Line Task++: Hello World

---
+++ 2-kyu/Multi-Line-Task++:-Hello-World.js
@@ -0,0 +1,40 @@
+/**
+ * 2 kyu - Multi Line Task++: Hello World
+ * https://www.codewars.com/kata/5935558a32fb828aad001213
+ */
+f=
+''
+[
+'\
+t\
+o\
+S\
+t\
+r\
+i\
+n\
+g\
+']
+[
+'\
+b\
+i\
+n\
+d\
+']
+( 
+'\
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!\
+')
