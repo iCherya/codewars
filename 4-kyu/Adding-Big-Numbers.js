1caa9bfda27b027fed15c873a79a16815876c357
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-26 07:10:26 +0200

    4 kyu - Adding Big Numbers

---
+++ 4-kyu/Adding-Big-Numbers.js
@@ -0,0 +1,23 @@
+/**
+ * 4 kyu - Adding Big Numbers
+ * https://www.codewars.com/kata/525f4206b73515bffb000b21
+ */
+const add = (num1, num2) => {
+    const [str1, str2] = [num1, num2].map((el) => el.split('').reverse().join(''));
+    const len = num1.length > num2.length ? num1.length : num2.length;
+    const result = [];
+    let rest = 0;
+
+    for (let i = 0; i < len; i++) {
+        const sum = (+str1[i] || 0) + (+str2[i] || 0) + rest;
+        rest = sum > 9 ? Math.floor(sum / 10) : 0;
+
+        result.push(sum % 10);
+    }
+
+    if (rest !== 0) {
+        result.push(rest);
+    }
+
+    return result.reverse().join('');
+};
