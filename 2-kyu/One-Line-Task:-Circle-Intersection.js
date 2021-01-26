95cb8b730a38b9a146a9dafd506859015aef7bd2
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-26 07:29:52 +0200

    2 kyu - One Line Task: Circle Intersection

---
+++ 2-kyu/One-Line-Task:-Circle-Intersection.js
@@ -0,0 +1,5 @@
+/**
+ * 2 kyu - One Line Task: Circle Intersection
+ * https://www.codewars.com/kata/5908242330e4f567e90000a3
+ */
+with(Math)circleIntersection=([a,b],[c,d],r)=>(r*r*(-sin(D=2*acos(hypot(a-c,b-d)/2/r))+D))|0
