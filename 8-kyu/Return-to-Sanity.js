787fb9a2f4b7bd458429acdc63299e2cbc383561
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-27 19:08:42 +0200

    8 kyu - Return to Sanity

---
+++ 8-kyu/Return-to-Sanity.js
@@ -0,0 +1,8 @@
+/**
+ * 8 kyu - Return to Sanity
+ * https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+ */
+const mystery = () => {
+    const results = { sanity: 'Hello' };
+    return results;
+};
