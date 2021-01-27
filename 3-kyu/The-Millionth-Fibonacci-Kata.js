938c784d1defd3bcddcc1df9d6031f815eea765a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-27 08:31:35 +0200

    3 kyu - The Millionth Fibonacci Kata

---
+++ 3-kyu/The-Millionth-Fibonacci-Kata.js
@@ -0,0 +1,29 @@
+/**
+ * 3 kyu - The Millionth Fibonacci Kata
+ * https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
+ */
+const fib = (n) => {
+    if (n === 0 || n === 1) return BigInt(n);
+
+    if (n >= 2 && n % 2 == 0) {
+        let k = n / 2;
+        let fk = fib(k);
+
+        return (2n * fib(k - 1) + fk) * fk;
+    }
+
+    if (n >= 2) {
+        let k = (n + 1) / 2;
+        let fk1 = fib(k - 1);
+        let fk2 = fib(k);
+        return fk2 * fk2 + fk1 * fk1;
+    }
+
+    let [a, b] = [0n, 1n];
+
+    for (let i = 0; i > n; i--) {
+        [a, b] = [b - a, a];
+    }
+
+    return a;
+};
