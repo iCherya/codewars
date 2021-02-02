0707ef7142e6d0422e9bb1c5b0f8cb777a7e22a3
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-02 07:36:48 +0200

    7 kyu - Is this working

---
+++ 7-kyu/Is-this-working?.js
@@ -0,0 +1,12 @@
+/**
+ * 7 kyu - Is this working?
+ * https://www.codewars.com/kata/559f8d487fa8511c43000118
+ */
+class Counter {
+    constructor() {
+        this.count = 0;
+    }
+    updateCount() {
+        this.count++;
+    }
+}
