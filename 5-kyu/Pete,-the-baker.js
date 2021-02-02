5792f76e314cef490bf3a5fc158356d8cdf7db03
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-02 08:02:52 +0200

    5 kyu - Pete, the baker

---
+++ 5-kyu/Pete,-the-baker.js
@@ -0,0 +1,16 @@
+/**
+ * 5 kyu - Pete, the baker
+ * https://www.codewars.com/kata/525c65e51bf619685c000059
+ */
+function cakes(recipe, available) {
+    const quantity = [];
+
+    for (const ingredient in recipe) {
+        if (!available[ingredient]) return 0;
+
+        const q = Math.floor(available[ingredient] / recipe[ingredient]);
+        quantity.push(q);
+    }
+
+    return Math.min(...quantity);
+}
