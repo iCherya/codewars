4cdce2b53f129590147aa8c95d018345d95ca224
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-04 08:07:46 +0200

    8 kyu - Find the smallest integer in the array

---
+++ 8-kyu/Find-the-smallest-integer-in-the-array.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Find the smallest integer in the array
+ * https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+ */
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args);
+    }
+}
