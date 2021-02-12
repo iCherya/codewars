9cd87cc7a7ef636526642517f1d6e76f86157bd3
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-12 20:06:38 +0200

    7 kyu - Evens times last

---
+++ 7-kyu/Evens-times-last.js
@@ -0,0 +1,7 @@
+/**
+ * 7 kyu - Evens times last
+ * https://www.codewars.com/kata/5a1a9e5032b8b98477000004
+ */
+const evenLast = (numbers) =>
+    (numbers[numbers.length - 1] || 0) *
+    numbers.filter((el, idx) => idx % 2 == 0).reduce((acc, curr) => acc + curr, 0);
