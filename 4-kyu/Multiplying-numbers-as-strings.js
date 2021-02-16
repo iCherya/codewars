f264292ebb17b438de482fd4c9dcd3bf8d57fbad
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-16 07:22:54 +0200

    4 kyu - Multiplying numbers as strings

---
+++ 4-kyu/Multiplying-numbers-as-strings.js
@@ -0,0 +1,22 @@
+/**
+ * 4 kyu - Multiplying numbers as strings
+ * https://www.codewars.com/kata/55911ef14065454c75000062
+ */
+const multiply = (num1, num2) => {
+    if (num1 === '0' || num2 === '0') return '0';
+
+    const product = Array(num1.length + num2.length).fill(0);
+    let last = product.length - 1;
+
+    for (let i = num1.length - 1; i >= 0; i--) {
+        let currIdx = last--;
+
+        for (let j = num2.length - 1; j >= 0; j--) {
+            const res = +num1[i] * +num2[j] + product[currIdx];
+            product[currIdx] = res % 10;
+            product[--currIdx] += Math.floor(res / 10);
+        }
+    }
+
+    return product.join('').replace(/^0+/, '');
+};
