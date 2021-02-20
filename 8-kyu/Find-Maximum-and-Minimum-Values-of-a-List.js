ad1820bf3bd07d5ac1102861f929255178e88376
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-20 08:16:33 +0200

    8 kyu - Find Maximum and Minimum Values of a List

---
+++ 8-kyu/Find-Maximum-and-Minimum-Values-of-a-List.js
@@ -0,0 +1,7 @@
+/**
+ * 8 kyu - Find Maximum and Minimum Values of a List
+ * https://www.codewars.com/kata/577a98a6ae28071780000989
+ */
+const min = (list) => Math.min(...list);
+
+const max = (list) => Math.max(...list);
