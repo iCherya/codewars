dd1a21cc30f632b3e3349324f0dcb7e668f1b00e
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-21 11:21:46 +0200

    8 kyu - Circular Objects #1 - Running around in circles

---
+++ 8-kyu/Circular-Objects-#1-Running-around-in-circles.js
@@ -0,0 +1,10 @@
+/**
+ * 8 kyu - Circular Objects #1 - Running around in circles
+ * https://www.codewars.com/kata/57078c56924f34f763000b3f
+ */
+
+const circular = {
+    value: `Hello World`
+};
+
+circular.self = circular;
