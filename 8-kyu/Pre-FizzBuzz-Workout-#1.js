e696104ba8b7d168fe4a55963bca3adfb725d39b
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-21 11:12:22 +0200

    8 kyu - Pre-FizzBuzz Workout #1

---
+++ 8-kyu/Pre-FizzBuzz-Workout-#1.js
@@ -0,0 +1,13 @@
+/**
+ * 8 kyu - Pre-FizzBuzz Workout #1
+ * https://www.codewars.com/kata/569e09850a8e371ab200000b
+ */
+const preFizz = (n) => {
+    const result = [];
+
+    for (let i = 1; i <= n; i++) {
+        result.push(i);
+    }
+
+    return result;
+};
