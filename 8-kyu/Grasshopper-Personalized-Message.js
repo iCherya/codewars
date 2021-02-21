81342568e66dfd7d7e6fd3a01c24fe5d8118576a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-21 11:14:56 +0200

    8 kyu - Grasshopper - Personalized Message

---
+++ 8-kyu/Grasshopper-Personalized-Message.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Grasshopper - Personalized Message
+ * https://www.codewars.com/kata/5772da22b89313a4d50012f7
+ */
+const greet = (name, owner) => 'Hello ' + (name === owner ? 'boss' : 'guest');
