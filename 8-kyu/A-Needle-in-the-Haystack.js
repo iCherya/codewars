c55eedbe4752426bada22470a888a9ac8e98efe9
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-23 09:55:54 +0200

    8 kyu - A Needle in the Haystack

---
+++ 8-kyu/A-Needle-in-the-Haystack.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - A Needle in the Haystack
+ * https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+ */
+const findNeedle = (haystack) => 'found the needle at position ' + haystack.indexOf('needle');
