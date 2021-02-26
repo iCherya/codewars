adfb970aa787bfc9ac90f70bc0810c56ebec9d63
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-26 08:14:54 +0200

    8 kyu - Get Planet Name By ID

---
+++ 8-kyu/Get-Planet-Name-By-ID.js
@@ -0,0 +1,36 @@
+/**
+ * 8 kyu - Get Planet Name By ID
+ * https://www.codewars.com/kata/515e188a311df01cba000003
+ */
+const getPlanetName = (id) => {
+    let name;
+
+    switch (id) {
+        case 1:
+            name = 'Mercury';
+            break;
+        case 2:
+            name = 'Venus';
+            break;
+        case 3:
+            name = 'Earth';
+            break;
+        case 4:
+            name = 'Mars';
+            break;
+        case 5:
+            name = 'Jupiter';
+            break;
+        case 6:
+            name = 'Saturn';
+            break;
+        case 7:
+            name = 'Uranus';
+            break;
+        case 8:
+            name = 'Neptune';
+            break;
+    }
+
+    return name;
+};
