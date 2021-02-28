a220309f2427b8a59dec09f83c39d7104ee045c0
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-28 12:23:54 +0200

    8 kyu - Volume of a Cuboid

---
+++ 8-kyu/Volume-of-a-Cuboid.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Volume of a Cuboid
+ * https://www.codewars.com/kata/58261acb22be6e2ed800003a
+ */
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height;
+    }
+}
