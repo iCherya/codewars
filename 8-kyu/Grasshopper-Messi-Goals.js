4fbfcd88cde171c117ada7d82a71d43ec4cf1fa3
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-07 08:51:27 +0200

    8 kyu - Grasshopper - Messi Goals

---
+++ 8-kyu/Grasshopper-Messi-Goals.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Grasshopper - Messi Goals
+ * https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+ */
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
