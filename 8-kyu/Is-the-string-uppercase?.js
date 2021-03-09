72d5484c10f68b0c9a52a03117143390c88a3795
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-09 10:32:21 +0200

    8 kyu - Is the string uppercase?

---
+++ 8-kyu/Is-the-string-uppercase?.js
@@ -0,0 +1,7 @@
+/**
+ * 8 kyu - Is the string uppercase?
+ * https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+ */
+String.prototype.isUpperCase = function () {
+    return this.toUpperCase() == this;
+};
