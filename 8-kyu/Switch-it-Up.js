5b26d1810a68f540ae42eb273035c7ba135cb88a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-16 07:22:56 +0200

    8 kyu - Switch it Up

---
+++ 8-kyu/Switch-it-Up.js
@@ -0,0 +1,6 @@
+/**
+ * 8 kyu - Switch it Up!
+ * https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+ */
+const switchItUp = (number) =>
+    ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number];
