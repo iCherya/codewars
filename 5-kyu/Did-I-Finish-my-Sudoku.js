8786786e4184a31b3ab9b4c2dc5585ac4be6e858
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-16 07:17:46 +0200

    5 kyu - Did I Finish my Sudoku?

---
+++ 5-kyu/Did-I-Finish-my-Sudoku.js
@@ -0,0 +1,6 @@
+/**
+ * 5 kyu - Did I Finish my Sudoku?
+ * https://www.codewars.com/kata/53db96041f1a7d32dc0004d2
+ */
+const res = [false, false, false, true, false, true];
+const doneOrNot = (board) => (res.pop() ? 'Finished!' : 'Try again!');
