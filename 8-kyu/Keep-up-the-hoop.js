ae4e754f6f2936973672fd9ae6e3754c866051fa
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-17 09:27:13 +0200

    8 kyu - Keep up the hoop

---
+++ 8-kyu/Keep-up-the-hoop.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Keep up the hoop
+ * https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+ */
+const hoopCount = (n) => (n > 9 ? 'Great, now move on to tricks' : 'Keep at it until you get it');
