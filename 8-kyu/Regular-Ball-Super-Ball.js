e159e06058d4823d90c988e427c1b98e00435c91
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-18 08:10:35 +0200

    8 kyu - Regular Ball Super Ball

---
+++ 8-kyu/Regular-Ball-Super-Ball.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Regular Ball Super Ball
+ * https://www.codewars.com/kata/53f0f358b9cb376eca001079
+ */
+class Ball {
+    constructor(ballType = 'regular') {
+        this.ballType = ballType;
+    }
+}
