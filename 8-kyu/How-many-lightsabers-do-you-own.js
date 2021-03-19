a35a8aa409668bfdcd6596440bd60f2c9582ee29
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-19 09:29:31 +0200

    8 kyu - How many lightsabers do you own?

---
+++ 8-kyu/How-many-lightsabers-do-you-own.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - How many lightsabers do you own?
+ * https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+ */
+const howManyLightsabersDoYouOwn = (name) => (name === 'Zach' ? 18 : 0);
