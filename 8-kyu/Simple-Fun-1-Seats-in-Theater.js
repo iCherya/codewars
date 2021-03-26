c45e34a8e083213aa12860dfd532b6727020bb25
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-26 09:44:15 +0200

    8 kyu - Simple Fun #1: Seats in Theater

---
+++ 8-kyu/Simple-Fun-1-Seats-in-Theater.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Simple Fun #1: Seats in Theater
+ * https://www.codewars.com/kata/588417e576933b0ec9000045
+ */
+const seatsInTheater = (nCols, nRows, col, row) => (nCols - col + 1) * (nRows - row);
