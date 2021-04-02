726b1ab9f37fae4bc3652a4c865cb0fcf0aef4a3
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-02 09:57:44 +0300

    8 kyu - No zeros for heros

---
+++ 8-kyu/No-zeros-for-heros.js
@@ -0,0 +1,13 @@
+/**
+ * 8 kyu - No zeros for heros
+ * https://www.codewars.com/kata/570a6a46455d08ff8d001002
+ */
+const noBoringZeros = (n) => {
+    if (n === 0) return n;
+
+    while (n % 10 === 0) {
+        n = n / 10;
+    }
+
+    return n;
+};
