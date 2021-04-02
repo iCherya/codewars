9404d7ac49bf0f7d65f08e664b6df26cc634e536
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-02 10:03:24 +0300

    8 kyu - Twice as old

---
+++ 8-kyu/Twice-as-old.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Twice as old
+ * https://www.codewars.com/kata/5b853229cfde412a470000d0
+ */
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - 2 * sonYearsOld);
