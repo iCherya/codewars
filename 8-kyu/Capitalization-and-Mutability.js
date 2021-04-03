7e03c4555660a2fd2e07c8fdc5b36a32f922ef43
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-03 10:38:49 +0300

    8 kyu - Capitalization and Mutability

---
+++ 8-kyu/Capitalization-and-Mutability.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Capitalization and Mutability
+ * https://www.codewars.com/kata/595970246c9b8fa0a8000086
+ */
+const capitalizeWord = (word) => word[0].toUpperCase() + word.slice(1);
