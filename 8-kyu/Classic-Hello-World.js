79ce1c482ce5cf49d58f175c39b5c282adf9d9af
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-15 08:34:17 +0300

    8 kyu - Classic Hello World

---
+++ 8-kyu/Classic-Hello-World.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+/**
+ * 8 kyu - Classic Hello World
+ * https://www.codewars.com/kata/57036f007fd72e3b77000023
+ */
+class Solution {
+    static main() {
+        console.log('Hello World!');
+    }
+}
