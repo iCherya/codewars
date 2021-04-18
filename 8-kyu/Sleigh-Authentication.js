c69d189bfbb60b076c12d30d3be96adefeb6478f
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-18 09:15:01 +0300

    8 kyu - Sleigh Authentication

---
+++ 8-kyu/Sleigh-Authentication.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Sleigh Authentication
+ * https://www.codewars.com/kata/52adc142b2651f25a8000643
+ */
+class Sleigh {
+    authenticate(name, password) {
+        return name === 'Santa Claus' && password === 'Ho Ho Ho!';
+    }
+}
