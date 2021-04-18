3b582d79f3f17e7fbcdd3b9a38f4ba856b6f64d6
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-18 09:25:36 +0300

    8 kyu - Training JS #1: create your first JS function and print Hello World

---
+++ 8-kyu/Training-JS-#1-create-your-first-JS-function-and-print-Helloworld.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-var */
+/* eslint-disable no-console */
+/**
+ * 8 kyu - Training JS #1: create your first JS function and print "Hello World!"
+ * https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+ */
+function helloWorld() {
+    var str = 'Hello World!';
+
+    console.log(str);
+}
