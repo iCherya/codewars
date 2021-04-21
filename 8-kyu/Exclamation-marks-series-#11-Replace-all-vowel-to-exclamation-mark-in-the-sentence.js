49b447f967485b7613f113646b41adbf354045be
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-21 08:16:04 +0300

    8 kyu - Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence

---
+++ 8-kyu/Exclamation-marks-series-#11-Replace-all-vowel-to-exclamation-mark-in-the-sentence.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+ * https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+ */
+const replace = (s) => s.replace(/[aeoiu]/gi, '!');
