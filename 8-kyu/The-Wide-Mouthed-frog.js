3daec727409c53c5ac55a3d90aa1d97a821698a2
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-24 10:59:24 +0300

    8 kyu - The Wide-Mouthed frog

---
+++ 8-kyu/The-Wide-Mouthed-frog.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - The Wide-Mouthed frog!
+ * https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+ */
+const mouthSize = (animal) => (animal.toLowerCase() === 'alligator' ? 'small' : 'wide');
