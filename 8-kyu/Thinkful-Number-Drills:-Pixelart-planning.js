e90b1e86c6d62a8fb56028911adeda3e82d67ea9
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-29 07:56:39 +0300

    8 kyu - Thinkful - Number Drills: Pixelart planning

---
+++ 8-kyu/Thinkful-Number-Drills:-Pixelart-planning.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Thinkful - Number Drills: Pixelart planning
+ * https://www.codewars.com/kata/58630e2ae88af44d2b0000ea
+ */
+const isDivisible = (wallLength, pixelSize) => wallLength % pixelSize === 0;
