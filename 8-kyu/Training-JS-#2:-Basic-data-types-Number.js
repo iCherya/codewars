6976b97c91aaf4b997f3d821a28e3329bb7bf4be
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-04 17:35:19 +0300

    8 kyu - Training JS #2: Basic data types--Number

---
+++ 8-kyu/Training-JS-#2:-Basic-data-types-Number.js
@@ -0,0 +1,16 @@
+/**
+ * 8 kyu - Training JS #2: Basic data types--Number
+ * https://www.codewars.com/kata/571edd157e8954bab500032d
+ */
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+const equal1 = () => v1 + v1;
+const equal2 = () => v4 - v2;
+const equal3 = () => v1 * v5;
+const equal4 = () => v4 / v5;
+const equal5 = () => v6 % v3;
