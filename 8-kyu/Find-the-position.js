fc173f4c280239a7c099d0bd995360da8cb9b9b1
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-04 17:44:24 +0300

    8 kyu - Find the position

---
+++ 8-kyu/Find-the-position.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Find the position!
+ * https://www.codewars.com/kata/5808e2006b65bff35500008f
+ */
+const position = (letter) => 'Position of alphabet: ' + (letter.charCodeAt() - 96);
