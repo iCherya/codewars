b4afbbf04edd3d541e016f1b27387e6a124da8bf
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-10 08:06:55 +0300

    8 kyu - Multiplication table for number

---
+++ 8-kyu/Multiplication-table-for-number.js
@@ -0,0 +1,15 @@
+/**
+ * 8 kyu - Multiplication table for number
+ * https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+ */
+const multiTable = (number) => {
+    const result = [];
+
+    for (let i = 1; i <= 10; i++) {
+        const line = `${i} * ${number} = ${number * i}`;
+        result.push(line);
+    }
+
+    return result.join('\n');
+};
+console.log(multiTable(5));
