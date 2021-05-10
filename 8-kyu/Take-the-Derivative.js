12cf75b4daf0d6c2262b4a405568aea5a3ebbc1e
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-10 08:02:00 +0300

    8 kyu - Take the Derivative

---
+++ 8-kyu/Take-the-Derivative.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Take the Derivative
+ * https://www.codewars.com/kata/5963c18ecb97be020b0000a2
+ */
+const derive = (coefficient, exponent) => `${coefficient * exponent}x^${exponent - 1}`;
