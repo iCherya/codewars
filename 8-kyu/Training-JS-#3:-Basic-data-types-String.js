a4d8f019154872de03cbb22762fca9de0096b852
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-11 07:31:53 +0300

    8 kyu - Training JS #3: Basic data types--String

---
+++ 8-kyu/Training-JS-#3:-Basic-data-types-String.js
@@ -0,0 +1,23 @@
+/**
+ * 8 kyu - Training JS #3: Basic data types--String
+ * https://www.codewars.com/kata/571edea4b625edcb51000d8e
+ */
+let a1 = 'A',
+    a2 = 'a',
+    b1 = 'B',
+    b2 = 'b',
+    c1 = 'C',
+    c2 = 'c',
+    d1 = 'D',
+    d2 = 'd',
+    e1 = 'E',
+    e2 = 'e',
+    n1 = 'N',
+    n2 = 'n';
+
+const Dad = () => d1 + a2 + d2;
+const Bee = () => b1 + e2 + e2;
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2;
+const answer1 = () => 'no';
+const answer2 = () => 'no';
+const answer3 = () => 'yes';
