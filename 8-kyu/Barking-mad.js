1ec00963a6d8db569f20cbf71509ef0ba717ab23
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-14 07:20:34 +0300

    8 kyu - Barking mad

---
+++ 8-kyu/Barking-mad.js
@@ -0,0 +1,12 @@
+/**
+ * 8 kyu - Barking mad
+ * https://www.codewars.com/kata/54dba07f03e88a4cec000caf
+ */
+class Dog {
+    bark() {
+        return 'Woof';
+    }
+}
+
+const snoopy = new Dog('Beagle');
+const scoobydoo = new Dog('Great Dane');
