5d31170aa637b8061ce74f7aff65b11fab3d4d4a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-17 07:39:37 +0300

    8 kyu - Training JS #5: Basic data types--Object

---
+++ 8-kyu/Training-JS-#5:-Basic-data-types-Object.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Training JS #5: Basic data types--Object
+ * https://www.codewars.com/kata/571f1eb77e8954a812000837
+ */
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
