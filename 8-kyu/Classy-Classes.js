c6f824bfe3ce2be9b9979e66e897eac6808e53d5
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-18 08:01:02 +0300

    8 kyu - Classy Classes

---
+++ 8-kyu/Classy-Classes.js
@@ -0,0 +1,11 @@
+/**
+ * 8 kyu - Classy Classes
+ * https://www.codewars.com/kata/55a144eff5124e546400005a
+ */
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.info = `${this.name}s age is ${this.age}`;
+    }
+}
