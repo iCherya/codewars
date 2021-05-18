c96ecb5d5624a908604f7180ee926cc310e0735a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-18 07:57:13 +0300

    8 kyu - Find the Integral

---
+++ 8-kyu/Find-the-Integral.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Find the Integral
+ * https://www.codewars.com/kata/59811fd8a070625d4c000013
+ */
+const integrate = (coefficient, exponent) => `${coefficient / (exponent + 1)}x^${exponent + 1}`;
