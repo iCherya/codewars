f12a1d7733e75b3ba676acc1b4ffaecad16f9330
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-19 22:32:32 +0300

    8 kyu - Is there a vowel in there?

---
+++ 8-kyu/Is-there-a-vowel-in-there.js
@@ -0,0 +1,13 @@
+/**
+ * 8 kyu - Is there a vowel in there?
+ * https://www.codewars.com/kata/57cff961eca260b71900008f
+ */
+const isVow = (a) =>
+    a.map((number) => {
+        const letter = String.fromCharCode(number);
+
+        if (['a', 'e', 'i', 'o', 'u'].includes(letter)) return letter;
+
+        return number;
+    });
+);
