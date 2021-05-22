84fe30ecf31cc3c05797aca8476875283d13e4d5
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-22 09:00:41 +0300

    8 kyu - For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ 8-kyu/For-UFC-Fans-Total-Beginners-Conor-McGregor-vs-George-Saint-Pierre.js
@@ -0,0 +1,8 @@
+/**
+ * 8 kyu - For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+ * https://www.codewars.com/kata/582dafb611d576b745000b74
+ */
+const quote = (fighter) =>
+    fighter.toLowerCase() === 'george saint pierre'
+        ? 'I am not impressed by your performance.'
+        : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
