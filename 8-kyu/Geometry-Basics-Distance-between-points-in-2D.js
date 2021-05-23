25a3153430372e9806300e4e2b7aa981fac4211e
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-23 08:07:11 +0300

    8 kyu - Geometry Basics: Distance between points in 2D

---
+++ 8-kyu/Geometry-Basics-Distance-between-points-in-2D.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Geometry Basics: Distance between points in 2D
+ * https://www.codewars.com/kata/58dced7b702b805b200000be
+ */
+const distanceBetweenPoints = (a, b) => Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
