b19ef7bab7b5954431291fcee00530b7019d12e6
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-31 07:14:26 +0300

    8 kyu - String Templates - Bug Fixing #5

---
+++ 8-kyu/String-Templates-Bug-Fixing–#5.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - String Templates - Bug Fixing #5
+ * https://www.codewars.com/kata/55c90cad4b0fe31a7200001f
+ */
+const buildString = (...template) => `I like ${template.join(', ')}!`;
