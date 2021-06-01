fa9032ceba76dcbad46dd7483879456b4e72ab66
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-01 07:25:27 +0300

    8 kyu - Did she say hallo?

---
+++ 8-kyu/Did-she-say-hallo.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Did she say hallo?
+ * https://www.codewars.com/kata/56a4addbfd4a55694100001f
+ */
+const validateHello = (greetings) => /(hello|hallo|ciao|salut|hola|ahoj|czesc)/i.test(greetings);
