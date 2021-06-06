2c9e344f4ec47a169854596634aeb09dcb882c0a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-06 09:21:07 +0300

    8 kyu - Grasshopper - Create the rooms

---
+++ 8-kyu/Grasshopper-Create-the-rooms.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Grasshopper - Create the rooms
+ * https://www.codewars.com/kata/56a29b237e9e997ff2000048
+ */
+const rooms = {
+    1: { name: '1', description: '1', completed: true },
+    2: { name: '2', description: '2', completed: true },
+    3: { name: '3', description: '3', completed: true }
+};
