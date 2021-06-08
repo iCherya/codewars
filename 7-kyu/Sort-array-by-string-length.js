ef5e6f37b8c7285e712ab46542a0556fedad3180
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-08 07:23:38 +0300

    7 kyu - Sort array by string length

---
+++ 7-kyu/Sort-array-by-string-length.js
@@ -0,0 +1,5 @@
+/**
+ * 7 kyu - Sort array by string length
+ * https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c
+ */
+const sortByLength = (array) => array.sort((a, b) => a.length - b.length);
