492900b78da0d7c12b0952b2b7584a6494dd0144
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-11 07:05:27 +0300

    7 kyu - The Coupon Code

---
+++ 7-kyu/The-Coupon-Code.js
@@ -0,0 +1,6 @@
+/**
+ * 7 kyu - The Coupon Code
+ * https://www.codewars.com/kata/539de388a540db7fec000642
+ */
+const checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) =>
+    enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate);
