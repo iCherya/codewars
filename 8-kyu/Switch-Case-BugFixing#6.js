7401131629e4a620c1418429ac94bb9e4bd8ccf3
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-11 06:54:44 +0300

    8 kuy - Switch/Case - Bug Fixing #6

---
+++ 8-kyu/Switch-Case-BugFixing#6.js
@@ -0,0 +1,22 @@
+/**
+ * 8 kuy - Switch/Case - Bug Fixing #6
+ * https://www.codewars.com/kata/55c933c115a8c426ac000082
+ */
+const evalObject = (value) => {
+    switch (value.operation) {
+        case '+':
+            return value.a + value.b;
+        case '-':
+            return value.a - value.b;
+        case '/':
+            return value.a / value.b;
+        case '*':
+            return value.a * value.b;
+        case '%':
+            return value.a % value.b;
+        case '^':
+            return Math.pow(value.a, value.b);
+        default:
+            return;
+    }
+};
