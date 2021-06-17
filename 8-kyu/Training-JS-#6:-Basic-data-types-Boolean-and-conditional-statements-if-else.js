4c7f697d84c4131b43b2778970b1959c931e5d4d
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-17 08:23:30 +0300

    8 kyu - Training JS #6: Basic data types--Boolean and conditional statements if..else

---
+++ 8-kyu/Training-JS-#6:-Basic-data-types-Boolean-and-conditional-statements-if-else.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Training JS #6: Basic data types--Boolean and conditional statements if..else
+ * https://www.codewars.com/kata/571f832f07363d295d001ba8
+ */
+const trueOrFalse = (val) => '' + !!val;
