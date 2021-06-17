764719f80a95d1bd02cb07882605dc49ed689fb1
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-17 08:20:04 +0300

    8 kyu - Object Oriented Piracy

---
+++ 8-kyu/Object-Oriented-Piracy.js
@@ -0,0 +1,14 @@
+/**
+ * 8 kyu - Object Oriented Piracy
+ * https://www.codewars.com/kata/54fe05c4762e2e3047000add
+ */
+class Ship {
+    constructor(draft, crew) {
+        this.draft = draft;
+        this.crew = crew;
+    }
+
+    isWorthIt() {
+        return this.draft - this.crew * 1.5 > 20;
+    }
+}
