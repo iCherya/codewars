af289bb79e7dc85354ce26c23d16590a08e0138d
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-20 05:58:40 +0300

    7 kyu - Factorial - 2

--- 7-kyu/Factorial.js
+++ 7-kyu/Factorial.js
@@ -1,6 +1,7 @@
 /**
  * 7 kyu - Factorial
  * https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc
+ * https://www.codewars.com/kata/57a049e253ba33ac5e000212
  */
 const factorial = (n) => {
     if (n < 0 || n > 12) throw RangeError;
