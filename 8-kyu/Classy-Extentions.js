f952fd5a5ef7b2ed65b4e260277af71433678f4f
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-20 05:56:14 +0300

    8 kyu - Classy Extentions

---
+++ 8-kyu/Classy-Extentions.js
@@ -0,0 +1,10 @@
+/**
+ * 8 kyu - Classy Extentions
+ * https://www.codewars.com/kata/55a14aa4817efe41c20000bc
+ */
+// eslint-disable-next-line no-undef
+class Cat extends Animal {
+    speak() {
+        return `${this.name} meows.`;
+    }
+}
