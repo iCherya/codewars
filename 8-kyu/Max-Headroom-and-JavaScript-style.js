e84e0ef3c62865df9af721a8beff3fa0ea11fae9
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-06-29 08:07:25 +0300

    8 kyu - Max Headroom and JavaScript style

---
+++ 8-kyu/Max-Headroom-and-JavaScript-style.js
@@ -0,0 +1,6 @@
+/**
+ * 8 kyu - Max Headroom and JavaScript style
+ * https://www.codewars.com/kata/52a47dd7e950edabfa000391
+ */
+const getMax1 = () => ({ name: 'Max Headroom' });
+const getMax2 = () => ({ name: 'Max Headroom' });
