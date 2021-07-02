696317cd5c17ca49514290241038e43c9f240a41
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-02 07:26:20 +0300

    8 kyu - Online RPG: player to qualifying stage?

---
+++ 8-kyu/Online-RPG:-player-to-qualifying-stage.js
@@ -0,0 +1,8 @@
+/**
+ * 8 kyu - Online RPG: player to qualifying stage?
+ * https://www.codewars.com/kata/55849d76acd73f6cc4000087
+ */
+const playerRankUp = (points) =>
+    points >= 100
+        ? 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.'
+        : false;
