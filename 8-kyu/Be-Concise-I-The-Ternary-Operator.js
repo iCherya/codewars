01627e659f6dd00350fe6c1fe67ce24af85f5a95
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-14 08:15:38 +0300

    8 kyu - Be Concise I - The Ternary Operator

---
+++ 8-kyu/Be-Concise-I-The-Ternary-Operator.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-nested-ternary */
+/**
+ * 8 kyu - Be Concise I - The Ternary Operator
+ * https://www.codewars.com/kata/56f3f6a82010832b02000f38
+ */
+const describeAge = (a) =>
+    `You're a(n) ${a < 13 ? 'kid' : a < 18 ? 'teenager' : a < 65 ? 'adult' : 'elderly'}`;
