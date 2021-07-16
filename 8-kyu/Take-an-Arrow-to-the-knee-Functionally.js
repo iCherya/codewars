61ae709583f4c0dcf44629288b2df20191329bbb
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-16 07:42:08 +0300

    8 kyu - Take an Arrow to the knee, Functionally

---
+++ 8-kyu/Take-an-Arrow-to-the-knee-Functionally.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Take an Arrow to the knee, Functionally
+ * https://www.codewars.com/kata/559f3123e66a7204f000009f
+ */
+const ArrowFunc = (arr) => arr.map((num) => String.fromCharCode(num)).join('');
