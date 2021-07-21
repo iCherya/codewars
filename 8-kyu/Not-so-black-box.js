ad8145d8811f40a21dfcec8ff375059d4c3c4ad5
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-21 07:28:10 +0300

    8 kyu - Not so black box

---
+++ 8-kyu/Not-so-black-box.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable new-cap */
+/**
+ * 8 kyu - Not so black box
+ * https://www.codewars.com/kata/57080f21d531cd94950007eb
+ */
+console.log(blackBox.toString());
+JSopenSesame();
