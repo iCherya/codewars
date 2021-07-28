14931c6d269c76d9390fea17b1047606f7d76bbf
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-28 07:26:22 +0300

    8 kyu - Training JS #29: methods of arrayObject---concat() and join()

---
+++ 8-kyu/Training-JS-#29:-methods-of-arrayObject-concat()-and-join().js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu - Training JS #29: methods of arrayObject---concat() and join()
+ * https://www.codewars.com/kata/5731861d05d14d6f50000626/train/javascript
+ */
+const bigToSmall = (arr) =>
+    []
+        .concat(...arr)
+        .sort((a, b) => b - a)
+        .join('>');
