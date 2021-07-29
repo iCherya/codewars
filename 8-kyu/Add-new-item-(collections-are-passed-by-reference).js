c693e9d01368f02a55be4337d70f5e3f43ecba1d
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-29 07:27:16 +0300

    8 kyu - Add new item (collections are passed by reference)

---
+++ 8-kyu/Add-new-item-(collections-are-passed-by-reference).js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Add new item (collections are passed by reference)
+ * https://www.codewars.com/kata/566dc05f855b36a031000048
+ */
+const addExtra = (listOfNumbers) => [...listOfNumbers, 4];
