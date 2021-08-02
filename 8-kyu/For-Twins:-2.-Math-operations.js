cee2c7cc4b85e945c79af2457c721f0d752b23f9
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-02 08:01:10 +0300

    8 kyu - For Twins: 2. Math operations

---
+++ 8-kyu/For-Twins:-2.-Math-operations.js
@@ -0,0 +1,6 @@
+/**
+ * 8 kyu - For Twins: 2. Math operations
+ * https://www.codewars.com/kata/59c287b16bddd291c700009a
+ */
+const iceBrickVolume = (radius, bottleLength, rimLength) =>
+    2 * (bottleLength - rimLength) * radius ** 2;
