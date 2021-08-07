35544009fe5b884a1686a13dac94b83d866364ff
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-07 07:52:11 +0300

    8 kyu - noobCode 02: TRICKY QUESTIONS ( primitives and operator precedence)

---
+++ 8-kyu/noobCode-02:-TRICKY-QUESTIONS.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - noobCode 02: TRICKY QUESTIONS ( primitives and operator precedence)
+ * https://www.codewars.com/kata/572269697297444c570008b9
+ */
+const greaterThanLessThan = (a, b, c) => a < b < c;
