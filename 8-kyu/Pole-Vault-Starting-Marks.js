93ff6043e2f741d2f2a617ca2911259c20796005
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-07 07:49:47 +0300

    8 kyu - Pole Vault Starting Marks

---
+++ 8-kyu/Pole-Vault-Starting-Marks.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Pole Vault Starting Marks
+ * https://www.codewars.com/kata/5786f8404c4709148f0006bf
+ */
+const startingMark = (bodyHeight) => Math.round((bodyHeight * 3.93432 + 3.4701) * 100) / 100;
