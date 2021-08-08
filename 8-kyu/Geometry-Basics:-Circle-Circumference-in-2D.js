fab9b38ff8b206051a28c32ce0cef4e7b73f7c54
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-08 09:03:45 +0300

    8 kyu - Geometry Basics: Circle Circumference in 2D

---
+++ 8-kyu/Geometry-Basics:-Circle-Circumference-in-2D.js
@@ -0,0 +1,5 @@
+/**
+ * 8 kyu - Geometry Basics: Circle Circumference in 2D
+ * https://www.codewars.com/kata/58e43389acfd3e81d5000a88
+ */
+const circleCircumference = ({ radius }) => 2 * Math.PI * radius;
