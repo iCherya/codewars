f76cf2d9cb70575a9b534e861cc0a05cfd002722
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-10 08:13:03 +0300

    7 kyu - JavaScript Array Filter

---
+++ 7-kyu/JavaScript-Array-Filter.js
@@ -0,0 +1,5 @@
+/**
+ * 7 kyu - JavaScript Array Filter
+ * https://www.codewars.com/kata/514a6336889283a3d2000001
+ */
+const getEvenNumbers = (numbersArray) => numbersArray.filter((el) => el % 2 === 0);
