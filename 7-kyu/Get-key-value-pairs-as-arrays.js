45490c7b6822ff5e0e2267c2c3ad79dbf1dcd64f
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-14 08:20:56 +0300

    7 kyu - Get key/value pairs as arrays

---
+++ 7-kyu/Get-key-value-pairs-as-arrays.js
@@ -0,0 +1,5 @@
+/**
+ * 7 kyu - Get key/value pairs as arrays
+ * https://www.codewars.com/kata/515dfd2f1db09667a0000003
+ */
+const keysAndValues = (data) => [Object.keys(data), Object.values(data)];
