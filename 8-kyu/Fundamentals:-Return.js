4dee32b3ad8b04997cc59db3e63b9c680c82f9fe
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-19 07:29:31 +0300

    8 kyu - Fundamentals: Return

---
+++ 8-kyu/Fundamentals:-Return.js
@@ -0,0 +1,10 @@
+/**
+ * 8 kyu - Fundamentals: Return
+ * https://www.codewars.com/kata/55a5befdf16499bffb00007b
+ */
+const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+const subt = (a, b) => a - b;
