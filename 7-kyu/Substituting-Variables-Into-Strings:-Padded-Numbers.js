53af96fd9a7621a1ba59ab65d57facdfee71c139
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-24 08:53:48 +0300

    7 kyu - Substituting Variables Into Strings: Padded Numbers

---
+++ 7-kyu/Substituting-Variables-Into-Strings:-Padded-Numbers.js
@@ -0,0 +1,5 @@
+/**
+ * 7 kyu - Substituting Variables Into Strings: Padded Numbers
+ * https://www.codewars.com/kata/51c89385ee245d7ddf000001
+ */
+const solution = (value) => `Value is ${value.toString().padStart(5, '0')}`;
