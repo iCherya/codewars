ccb1298ced0ef4ade00ade1c7456bbc16fde6a62
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-09-10 07:52:44 +0300

    7 kyu - Use reduce() to calculate the sum of the values in an array

---
+++ 7-kyu/Use-reduce-to-calculate-the-sum-of-the-values-in-an-array.js
@@ -0,0 +1,5 @@
+/**
+ * 7 kyu - Use reduce() to calculate the sum of the values in an array
+ * https://www.codewars.com/kata/532b4057484b0e58e8000766
+ */
+const sum = (array) => array.reduce((a, c) => a + c, 0);
